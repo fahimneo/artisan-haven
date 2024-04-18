08c1a38d1cac619dfc5d5af2efb771806ff13972
Rakibul Islam <eb191060@ugrad.iiuc.ac.bd>
2024-04-18 19:06:18 +0600

    Responsive added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAfuCA0o7vno59Yxs4ErOrVATx5_SPGsAQ",
-  authDomain: "a9-real-estate-1e537.firebaseapp.com",
-  projectId: "a9-real-estate-1e537",
-  storageBucket: "a9-real-estate-1e537.appspot.com",
-  messagingSenderId: "576410305342",
-  appId: "1:576410305342:web:3274943eb15870f808e26f"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
